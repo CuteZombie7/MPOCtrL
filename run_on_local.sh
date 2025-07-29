f1050a9acfa9fc5c4be50c7ebf57ed8db4716fbe
CuteZombie7 <yuheming0710@gmail.com>
2025-07-29 22:01:54 +0800

    add new genes as network input

--- run_on_local.sh
+++ run_on_local.sh
@@ -14,5 +14,6 @@ uv run src/main.py \
     --gene_expression_file_name Tabula_Sapiens_Biohub_pseudo_bulk_counts_output_cpm_log1p.csv.gz \
     --compounds_reactions_file_name GGSL_V3_cmMat.csv \
     --reactions_genes_file_name GGSL_V3_reactions_genes.json \
+	--reactions_auxiliary_genes_file_name close_gene_pairs_mi_GGSLV3.json
     --n_epoch 200 \
 	(--add_dup_auxiliary_genes)
